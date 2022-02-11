cf87b65663d247ab2b7f872e3fd490c9fff7c9fb
FellSoma <n.felix2003@gmail.com>
2022-02-11 18:54:33 +0500

    Add project.

---
+++ ProfOlimp/ProfOlimp/ProfOlimp/GeneralWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProfOlimp
+{
+    /// <summary>
+    /// Логика взаимодействия для GeneralWindow.xaml
+    /// </summary>
+    public partial class GeneralWindow : Window
+    {
+        public GeneralWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateMoney(object sender, RoutedEventArgs e)
+        {
+            Window w = new CalculateMoney();
+            w.Show();
+        }
+    }
+}
