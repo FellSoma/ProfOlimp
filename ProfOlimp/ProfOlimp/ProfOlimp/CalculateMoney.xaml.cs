6321b53a780bbb7a90b475aae85fa18a446ba237
Firmai25 <den.cherepanov.03@mail.ru>
2022-02-11 21:18:56 +0500

    All forms are centered

--- ProfOlimp/ProfOlimp/ProfOlimp/CalculateMoney.xaml.cs
+++ ProfOlimp/ProfOlimp/ProfOlimp/CalculateMoney.xaml.cs
@@ -22,6 +22,62 @@ namespace ProfOlimp
         public CalculateMoney()
         {
             InitializeComponent();
+
+
+         
         }
+        Int64 money,days,addmoney;
+        double Stabile;
+        double Optimale;
+
+        private void Exemple(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                days = Convert.ToInt32(slDays.Value);
+                money = Convert.ToInt32(Money.Text);
+                addmoney = Convert.ToInt32(addMoney.Text);
+
+            }
+            catch (Exception)
+            {
+
+               
+            }
+
+            try
+            {
+                 Stabile = Convert.ToDouble((money * 8 * days/365)/100);
+                 Optimale = Convert.ToDouble((money * 5 * days / 365) / 100);
+                 Standart = Convert.ToDouble((money * 6 * days / 365) / 100);
+
+            }
+            catch (Exception)
+            {
+
+               
+            }
+            try
+            {
+
+                tbStability.Text = "0";
+                tbOptimal.Text = "0";
+                tbStandart.Text = "0";
+
+                tbStability.Text = Stabile.ToString();
+                tbOptimal.Text = Optimale.ToString();
+                tbStandart.Text = Standart.ToString();
+            
+            }
+            catch (Exception)
+            {
+
+                
+            }
+        }
+
+        double Standart;
+      
     }
 }
--- ProfOlimp/ProfOlimp/ProfOlimp/ComparisonOfparameters.xaml.cs
+++ ProfOlimp/ProfOlimp/ProfOlimp/ComparisonOfparameters.xaml.cs
@@ -23,5 +23,10 @@ namespace ProfOlimp
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
